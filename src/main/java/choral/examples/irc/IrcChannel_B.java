d3c096e6c260f91231ea373641b44153d0fb31b2
lovrosdu <lugovic@imada.sdu.dk>
2022-04-28 10:51:40 +0200

    Fix missing public

--- src/main/java/choral/examples/irc/IrcChannel_B.java
+++ src/main/java/choral/examples/irc/IrcChannel_B.java
@@ -4,7 +4,7 @@ import java.nio.channels.ByteChannel;
 import choral.channels.SymChannel_B;
 
 public class IrcChannel_B extends IrcChannelImpl implements SymChannel_B<Message> {
-    IrcChannel_B(ByteChannel channel) {
+    public IrcChannel_B(ByteChannel channel) {
         super(channel);
     }
 }
