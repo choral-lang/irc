e52e54ed076e9165bb2a247dbe547fdbd37a0cb3
lovrosdu <lugovic@imada.sdu.dk>
2022-05-20 17:31:48 +0200

    Fix not starting all of the event loops as separate tasks

--- src/main/java/choral/examples/irc/Irc.java
+++ src/main/java/choral/examples/irc/Irc.java
@@ -4,17 +4,13 @@ import java.util.concurrent.ExecutorService;
 
 public class Irc {
     public static void runClient(Irc_Client client, ExecutorService executor) {
-        executor.submit(() -> {
-            client.clientDrivenLoop();
-            client.serverDrivenLoop();
-        });
+        executor.execute(client::clientDrivenLoop);
+        executor.execute(client::serverDrivenLoop);
     }
 
     public static void runServer(Irc_Server server, ExecutorService executor) {
-        executor.submit(() -> {
-            server.clientDrivenLoop();
-            server.serverDrivenLoop();
-            server.serverLocalLoop();
-        });
+        executor.execute(server::clientDrivenLoop);
+        executor.execute(server::serverDrivenLoop);
+        executor.execute(server::serverLocalLoop);
     }
 }
