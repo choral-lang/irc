bb2590f616b078500fb73a43be6ffb1e2c700287
lovrosdu <lugovic@imada.sdu.dk>
2022-06-01 16:38:56 +0200

    Underscore is also a valid nickname character

--- src/main/java/choral/examples/irc/Util.java
+++ src/main/java/choral/examples/irc/Util.java
@@ -1,7 +1,16 @@
 package choral.examples.irc;
 
 public class Util {
+    /**
+     * Check whether a nickname is valid.
+     *
+     * A valid nickname starts with an ASCII letter and is followed by one or
+     * more of the following characters: 'a'-'z', 'A'-'Z', '0'-'9', '-', '_',
+     * '\', '`', '^', '[', ']', '{' or '}'.
+     *
+     * See RFC 1459, section 2.3.1.
+     */
     public static boolean validNickname(String nickname) {
-        return nickname.matches("[a-zA-Z0-9\\-\\\\`^\\[\\]{}]+");
+        return nickname.matches("[a-zA-z][a-zA-Z0-9\\-_\\\\`^\\[\\]{}]*");
     }
 }
